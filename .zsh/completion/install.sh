249fb30553e4ee5d322a20a625f47474ab87e75b
Pradip Caulagi <caulagi@gmail.com>
2017-09-27 18:52:52 +0200

    Create directory before downloading completion scripts

--- .zsh/completion/install.sh
+++ .zsh/completion/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -pv ~/.zsh/completion
+
 curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
 curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/completion/_docker
 curl -L https://git.zx2c4.com/password-store/plain/src/completion/pass.zsh-completion > ~/.zsh/completion/_pass
