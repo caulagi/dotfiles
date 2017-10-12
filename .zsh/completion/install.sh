bff8276855875b852e4ecaaa69f076d259b761a7
Pradip Caulagi <caulagi@gmail.com>
2017-10-12 11:55:37 +0200

    Add completion for rustup

--- .zsh/completion/install.sh
+++ .zsh/completion/install.sh
@@ -5,3 +5,4 @@ mkdir -pv ~/.zsh/completion
 curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
 curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/completion/_docker
 curl -L https://git.zx2c4.com/password-store/plain/src/completion/pass.zsh-completion > ~/.zsh/completion/_pass
+rustup completions zsh > ~/.zsh/completion/_rustup
