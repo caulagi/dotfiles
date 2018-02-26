30480175d1be1123df9e42ac051c74be72bf2e1a
Pradip Caulagi <caulagi@gmail.com>
2018-02-26 22:34:37 +0100

    Source aliases, so vim is nvim

--- fish/config.fish
+++ fish/config.fish
@@ -8,16 +8,14 @@ set -xg LANG en_US.UTF-8
 # don't show brew git information on brew commands
 set -xg HOMEBREW_NO_GITHUB_API 1
 
-# set -xg GPG_TTY ``$(tty)
 set -xg GPG_TTY (tty)
 set -xg OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
 set -xg OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
 
 # use gitignore when searching with fzf
-export FZF_DEFAULT_COMMAND='ag -g ""'
+# export FZF_DEFAULT_COMMAND='ag -g ""'
 
-source ~/Downloads/google-cloud-sdk/path.fish.inc
-
+# source ~/Downloads/google-cloud-sdk/path.fish.inc
 
 set -xg PROJECT_ID `gcloud config get-value project`
 set -xg HOST_IP 192.168.65.2
@@ -26,4 +24,5 @@ set -xg NNTPSERVER nntp.aioe.org
 set -xg GOPATH ~/go
 set -xg PATH $GOPATH/bin $PATH
 
-# source ~/.personal
+. ~/.alias
+. ~/.personal
