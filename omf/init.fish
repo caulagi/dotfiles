9c5e4f3fb228852e39ba6171a97e5377deb6b0af
Pradip Caulagi <caulagi@gmail.com>
2018-12-02 17:31:28 +0100

    Variables have to be set as global to work in a new shell

--- omf/init.fish
+++ omf/init.fish
@@ -1,31 +1,32 @@
-set -x theme_color_scheme zenburn
-set -x EDITOR nvim
-set -x LC_ALL en_US.UTF-8
-set -x LANG en_US.UTF-8
+set -xg theme_color_scheme zenburn
+set -xg theme_nerd_fonts yes
+set -xg fish_key_bindings fish_vi_key_bindings
+set -xg theme_show_exit_status no
 
+set -xg EDITOR nvim
+set -xg LC_ALL en_US.UTF-8
+set -xg LANG en_US.UTF-8
 
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
-set -x theme_nerd_fonts yes
-set -x fish_key_bindings fish_vi_key_bindings
+set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # don't show brew git information on brew commands
-set -x HOMEBREW_NO_GITHUB_API 1
+set -xg HOMEBREW_NO_GITHUB_API 1
 
-set -x GPG_TTY (tty)
-set -x OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
-set -x OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
+set -xg GPG_TTY (tty)
+set -xg OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
+set -xg OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
 
 # use gitignore when searching with fzf
 # export FZF_DEFAULT_COMMAND='ag -g ""'
 
 # source ~/Downloads/google-cloud-sdk/path.fish.inc
 
-set -x PROJECT_ID `gcloud config get-value project`
-set -x HOST_IP 192.168.65.2
-set -x NNTPSERVER nntp.aioe.org
+set -xg PROJECT_ID `gcloud config get-value project`
+set -xg HOST_IP 192.168.65.2
+set -xg NNTPSERVER nntp.aioe.org
 
-set -x GOPATH ~/go
-set -x PATH $GOPATH/bin $PATH
+set -xg GOPATH ~/go
+set -xg PATH $GOPATH/bin $PATH
 
 . ~/.alias
 . ~/.personal
