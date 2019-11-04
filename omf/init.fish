42024752165237b9c9ceb0a855d7709d73b458de
Pradip Caulagi <caulagi@gmail.com>
2019-11-04 07:49:59 +0100

    Test binaries exist before adding to PATH
    Also order the environment variables alphabetically
    
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- omf/init.fish
+++ omf/init.fish
@@ -4,28 +4,33 @@ set -xg fish_key_bindings fish_vi_key_bindings
 set -xg theme_show_exit_status no
 
 set -xg EDITOR nvim
-set -xg LC_ALL en_US.UTF-8
-set -xg LANG en_US.UTF-8
-
-set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
-
-# don"t show brew git information on brew commands
-set -xg HOMEBREW_NO_GITHUB_API 1
-
 set -xg GPG_TTY (tty)
-set -xg OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
-set -xg OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
-
+set -xg HOMEBREW_NO_GITHUB_API 1 # don"t show brew git information on brew commands
 set -xg HOST_IP 192.168.65.2
-
-set -xg GOPATH ~/src/go
-set -xg PATH $GOPATH/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $HOME/.cargo/bin $PATH /usr/local/sbin
+set -xg LANG en_US.UTF-8
+set -xg LC_ALL en_US.UTF-8
+set -xg OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
+set -xg OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
 set -xg TF_LOG INFO
 set -xg TF_LOG_PATH /tmp/terraform.log
+set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
 set -xg WEECHAT_HOME $XDG_CONFIG_HOME/weechat
 set -xg XKB_DEFAULT_LAYOUT se
 
-eval (direnv hook fish)
+
+if type -p go >/dev/null 2>&1
+    set -xg GOPATH ~/src/go
+    set -xg PATH $GOPATH/bin $PATH
+end
+
+if type -p yarn >/dev/null 2>&1
+    set -xg PATH $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $PATH
+end
+
+
+if type -p direnv >/dev/null 2>&1
+    eval (direnv hook fish)
+end
 
 source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 
