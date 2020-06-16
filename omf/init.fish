36b6f8506c9389e63ca2660f87c0efdd5b63c9cb
Pradip Caulagi <caulagi@gmail.com>
2020-06-16 20:48:11 +0200

    Add a local binary directory
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- omf/init.fish
+++ omf/init.fish
@@ -15,6 +15,7 @@ set -xg TF_LOG INFO
 set -xg TF_LOG_PATH /tmp/terraform.log
 set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
 set -xg WEECHAT_HOME $XDG_CONFIG_HOME/weechat
+set -xg PATH ~/bin $PATH
 
 if type -p go >/dev/null 2>&1
     set -xg GOPATH ~/src/go
