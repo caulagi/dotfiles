f9845bb728f37bfbda02bb1881b1b7d661ed5c77
Pradip Caulagi <caulagi@gmail.com>
2020-10-08 21:36:04 +0200

    Start sway on login
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- omf/init.fish
+++ omf/init.fish
@@ -51,3 +51,9 @@ if [ -d "$HOME/Library/Android/sdk" ];
 end
 
 zoxide init fish | source
+
+# If running from tty1 start sway
+set TTY1 (tty)
+if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
+    exec sway
+end
