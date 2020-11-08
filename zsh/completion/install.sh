863ffa886fc157daa0c06a88c7c9b9113823b8ef
Pradip Caulagi <caulagi@gmail.com>
2020-11-08 16:12:49 +0100

    zsh: generate completions when it is supported
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- zsh/completion/install.sh
+++ zsh/completion/install.sh
@@ -5,8 +5,5 @@ mkdir -pv ~/.zsh/completion
 curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
 curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/completion/_docker
 curl -L https://git.zx2c4.com/password-store/plain/src/completion/pass.zsh-completion > ~/.zsh/completion/_pass
-rustup completions zsh > ~/.zsh/completion/_rustup
-kops completion zsh > ~/.zsh/completion/_kops
-helm completion zsh > ~/.zsh/completion/_helm
 curl -L https://raw.githubusercontent.com/aws/aws-cli/develop/bin/aws_zsh_completer.sh > ~/.zsh/completion/aws_zsh_completer.sh
 pipenv --completion > ~/.zsh/completion/_pipenv
