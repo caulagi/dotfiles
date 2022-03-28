08b312eba761a501de40622866d090dc074488d0
Pradip Caulagi <caulagi@gmail.com>
2022-03-28 10:23:14 +0200

    More configuration for wezterm
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,4 +4,14 @@ return {
   color_scheme = "Gruvbox Dark",
   font = wezterm.font("FiraCode Nerd Font"),
   font_size = 14,
+  audible_bell = "Disabled",
+  scrollback_lines = 100000,
+  window_frame = {
+    font_size = 14.0,
+  },
+  -- keys = {
+  --   {key="+", mods="CTRL", action="IncreaseFontSize"},
+  --   {key="-", mods="CTRL", action="DecreaseFontSize"},
+  -- },
+  send_composed_key_when_left_alt_is_pressed = true,
 }
