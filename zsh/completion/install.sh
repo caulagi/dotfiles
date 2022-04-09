2cd9765e090bd59970d7753566ecbce44087d74d
Pradip Caulagi <caulagi@gmail.com>
2022-04-09 18:48:07 +0200

    Remove completions for docker and docker-compose
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- zsh/completion/install.sh
+++ zsh/completion/install.sh
@@ -2,8 +2,6 @@
 
 mkdir -pv ~/.zsh/completion
 
-curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
-curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ~/.zsh/completion/_docker
 curl -L https://git.zx2c4.com/password-store/plain/src/completion/pass.zsh-completion > ~/.zsh/completion/_pass
 curl -L https://raw.githubusercontent.com/aws/aws-cli/develop/bin/aws_zsh_completer.sh > ~/.zsh/completion/aws_zsh_completer.sh
 pipenv --completion > ~/.zsh/completion/_pipenv
