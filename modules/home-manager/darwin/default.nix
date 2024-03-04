ce4904c2a42273c9601fbf58155af5be9cf989cb
Pradip Caulagi <caulagi@gmail.com>
2024-03-04 22:20:25 +0100

    Start using nix for configuring macbooks 🤘🎉

---
+++ modules/home-manager/darwin/default.nix
@@ -0,0 +1,86 @@
+{ pkgs, ... }: {
+  # here go the darwin preferences and config items
+  programs.bash.enable = true;
+  environment = {
+    systemPackages = [ pkgs.coreutils ];
+    systemPath = [ "/opt/homebrew/bin" ];
+    pathsToLink = [ "/Applications" ];
+    variables.EDITOR = "nvim";
+  };
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  users.users.pradipcaulagi.home = "/Users/pradipcaulagi";
+
+  networking = {
+    hostName = "pcaulagi-cdon-macbook";
+  };
+
+
+  fonts.fontDir.enable = true;
+  fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
+  services.nix-daemon.enable = true;
+  system = {
+    defaults = {
+      finder.AppleShowAllExtensions = true;
+      finder._FXShowPosixPathInTitle = true;
+      # don't show desktop icons
+      finder.CreateDesktop = false;
+      # default to list view
+      finder.FXPreferredViewStyle = "Nlsv";
+      dock.autohide = true;
+      dock.autohide-delay = 0.01;
+      dock.autohide-time-modifier = 0.01;
+      dock.show-recents = false;
+
+      CustomSystemPreferences = {
+        NSGlobalDomain = {
+          NSWindowShouldDragOnGesture = true;
+        };
+      };
+
+      NSGlobalDomain = {
+        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.sound.beep.volume" = 0.0;
+
+        # full keyboard control
+        AppleKeyboardUIMode = 3;
+
+        # allow key repeat
+        ApplePressAndHoldEnabled = false;
+        # delay before repeating keystrokes
+        InitialKeyRepeat = 15; # normal minimum is 15 (225 ms)
+        # delay between repeated keystrokes upon holding a key
+        KeyRepeat = 2; # normal minimum is 2 (30 ms)
+        AppleShowAllExtensions = true;
+        # AppleShowScrollBars = "Automatic";
+
+        # Reduce window animations
+        NSWindowResizeTime = 0.1;
+
+        # window open/close animation
+        NSAutomaticWindowAnimationsEnabled = false;
+      };
+    };
+    # backwards compat; don't change
+    stateVersion = 4;
+
+    activationScripts.postActivation.text = ''sudo chsh -s ${pkgs.bashInteractive}/bin/bash''; # Since it's not possible to declare default shell, run this command after build
+
+  };
+
+  homebrew = {
+    enable = true;
+    caskArgs.no_quarantine = true;
+    # use home brew to install packages for spotlight to work
+    casks = [ "wezterm" ];
+  };
+}
---
+++ modules/home-manager/default.nix
@@ -0,0 +1,167 @@
+# https://nix-community.github.io/home-manager/options.html
+{ pkgs, ... }: {
+  home.stateVersion = "22.11";
+
+  home.packages = with pkgs; [
+    awscli2
+    ko
+    go
+    gzip
+    httpie
+    colima
+    curl
+    docker
+    duf
+    (google-cloud-sdk.withExtraComponents # gcloud components list
+      (with google-cloud-sdk.components; [
+        gke-gcloud-auth-plugin
+        gcloud-man-pages
+      ])
+    )
+    kubectl
+    fd
+    pre-commit
+    hugo
+    gosec
+    jq
+    less
+    nixpkgs-fmt
+    ripgrep
+    shellcheck
+    watch
+  ];
+  home.sessionVariables = {
+    HISTFILE = "$HOME/bash_history/$(date +%Y-%m)";
+    PAGER = "less";
+    CLICLOLOR = 1;
+    PATH = "$PATH:$HOME/go/bin";
+  };
+
+  # https://nix-community.github.io/home-manager/options.html
+  programs.zoxide = {
+    enable = true;
+    options = [ "--cmd j" ];
+    enableBashIntegration = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+
+    extraConfig = ''
+      lua require('gitsigns').setup()
+
+      set list listchars=nbsp:¬,tab:»·,trail:·,extends:>
+      set shiftwidth=4
+      set softtabstop=4
+      set tabstop=4
+      set wildmode=longest,list,full
+      set wildmenu
+      autocmd ColorScheme * highlight Whitespace ctermfg=red guifg=#FF0000
+      autocmd BufWritePre * :%s/\s\+$//e
+      colorscheme dracula
+      set undofile
+      set ignorecase
+      unmap Y
+
+      " Jump to last cursor position when opening files
+      " See |last-position-jump|.
+      :au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      vim-commentary
+      vim-fugitive
+      vim-nix
+      vim-rhubarb
+      copilot-vim
+      dracula-vim
+      gitsigns-nvim
+    ];
+
+  };
+
+  programs.fzf.enable = true;
+  programs.fzf.enableBashIntegration = true;
+  programs.eza.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Pradip Caulagi";
+    userEmail = "caulagi@gmail.com";
+    aliases = {
+      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      root = "rev-parse --show-toplevel";
+    };
+    extraConfig = {
+      color.ui = true;
+      github.user = "caulagi";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+    };
+  };
+
+  programs.bash.enable = true;
+  programs.bash.enableCompletion = true;
+
+  programs.direnv.enable = true;
+  programs.direnv.enableBashIntegration = true;
+
+  programs.bash.shellAliases = {
+    sloc = "scc -c --no-cocomo";
+    archlinux = "docker run -it --rm --platform linux/amd64 archlinux";
+    k = "kubecolor";
+    ll = "eza -alh --group-directories-first";
+    sts = "aws sts get-caller-identity";
+    nixswitch = "darwin-rebuild switch --flake ~/mac-nix/.#";
+    nixsearch = "nix search nixpkgs";
+    nixup = "pushd ~/mac-nix; nix flake update; nixswitch; popd";
+  };
+
+  programs.bash.initExtra = ''
+    PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+    shopt -s histappend
+
+    # search bash history
+    h () {
+      rg -a --sort path "$@" ~/bash_history/
+    }
+  '';
+
+  programs.starship.enable = true;
+  programs.starship.enableBashIntegration = true;
+  programs.starship.settings = {
+    gcloud.disabled = true; # no email address
+
+    git_commit.only_detached = false; # show hash of git commit
+
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        normal = {
+          family = "MesloLGS Nerd Font Mono";
+        };
+        size = 16;
+      };
+      selection = {
+        save_to_clipboard = true;
+      };
+      cursor = {
+        style = "Block";
+        unfocused_hollow = true;
+      };
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+}
