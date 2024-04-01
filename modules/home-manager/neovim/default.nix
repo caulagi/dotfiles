8096b5d24cf61ce977facd0ace9fa35537223df0
Pradip Caulagi <caulagi@gmail.com>
2024-04-01 12:45:59 +0200

    (nix): use lua based setup for neovim

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -4,34 +4,7 @@
   lib,
   ...
 }: {
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    defaultEditor = true;
-
-    plugins = with pkgs.vimPlugins; [
-      nvim-treesitter
-    ];
-
-    extraConfig = ''
-      set list listchars=nbsp:¬,tab:»·,trail:·,extends:>
-      set shiftwidth=4
-      set softtabstop=4
-      set tabstop=4
-      set wildmode=longest,list,full
-      set wildmenu
-      autocmd ColorScheme * highlight Whitespace ctermfg=red guifg=#FF0000
-      autocmd BufWritePre * :%s/\s\+$//e
-      colorscheme dracula
-      set undofile
-      set ignorecase
-      unmap Y
-
-      " Jump to last cursor position when opening files
-      " See |last-position-jump|.
-      :au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-    '';
-  };
+  programs.neovim.enable = true;
+  home.file.".config/nvim/init.lua".text = builtins.readFile ./init.lua;
+  home.file.".config/nvim/lua/plugins.lua".text = builtins.readFile ./plugins.lua;
 }
