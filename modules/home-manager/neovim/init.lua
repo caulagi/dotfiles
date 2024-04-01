055d1c9d3216c25f1a50823c89fd192f710fade0
Pradip Caulagi <caulagi@gmail.com>
2024-04-01 17:28:05 +0200

    (nix): use lua based setup for neovim

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -1,1 +1,25 @@
-require('plugins')
+local opt = vim.opt
+local fn = vim.fn
+
+opt.listchars = {
+    tab = "→ ",
+    eol = "¬",
+    trail = "⋅",
+    extends = "❯",
+    precedes = "❮",
+}
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.wildmode = longest,list,full
+opt.wildmenu = true
+opt.ignorecase = true
+opt.undofile = true
+
+if fn.executable("rg") then
+    -- if ripgrep installed, use that as a grepper
+    opt.grepprg = "rg --vimgrep --no-heading"
+    opt.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+    -- create autocmd to automatically open quickfix window when grepping
+    vim.cmd([[autocmd QuickFixCmdPost [^l]* nested cwindow]])
+end
