53e4a88e8529d6dd5a09f74a2d66f3cee446ecce
Pradip Caulagi <caulagi@gmail.com>
2024-04-02 22:46:31 +0200

    (nix): enable lsp plugins

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -5,9 +5,11 @@
   ...
 }: {
   programs.neovim.enable = true;
+
   home.file.".config/nvim/init.lua".text = builtins.readFile ./init.lua;
   home.file.".config/nvim/lua/config/autocmds.lua".text = builtins.readFile ./lua/config/autocmds.lua;
   home.file.".config/nvim/lua/config/keymaps.lua".text = builtins.readFile ./lua/config/keymaps.lua;
   home.file.".config/nvim/lua/config/lazy.lua".text = builtins.readFile ./lua/config/lazy.lua;
   home.file.".config/nvim/lua/config/options.lua".text = builtins.readFile ./lua/config/options.lua;
+  home.file.".config/nvim/lua/plugins/custom.lua".text = builtins.readFile ./lua/plugins/custom.lua;
 }
